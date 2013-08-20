4e4f195fabfa1cb7f10daa38ee7783d2cf1e9388
Jan Ferko <julyloov@gmail.com>
2013-08-20 16:22:28 +0200

    list refactored to set

--- src/main/scala/sk/drunkenpanda/bot/Bot.scala
+++ src/main/scala/sk/drunkenpanda/bot/Bot.scala
@@ -2,11 +2,9 @@ package sk.drunkenpanda.bot
 
 import java.net.Socket
 import sk.drunkenpanda.bot.io._
-import sk.drunkenpanda.bot.plugins._
+import sk.drunkenpanda.bot.plugins.PluginModule
 
 class Bot(client: IrcClient) {
-
-  val plugins = List(new EchoPlugin(), new PongPlugin())
 
   def connect(nickname: String, realname: String, channel: String)
     : ConnectionSource => Unit =
@@ -14,23 +12,13 @@ class Bot(client: IrcClient) {
           client.open(realname, nickname)(s)
           client.join(channel)(s)
         }
-   
-  def process(message: Message): List[Message] = 
-    for {    
-      p <- plugins
-      r <- p respond message
-    } yield r
-    
-  def send(messages: List[Message]): ConnectionSource => Unit =
+      
+  def send(messages: Set[Message]): ConnectionSource => Unit =
     s => messages map {message => client.send(message)(s)}
 
   def listen(): ConnectionSource => Stream[Message] = 
     s => read(s)
-  
-  def processStream(messages: Stream[Message])
-      : ConnectionSource => Stream[List[Message]] = 
-    s => messages map {msg => process(msg)}
-
+   
   private def read(source: ConnectionSource): Stream[Message] = 
     client.receive()(source) #:: read(source)
       
