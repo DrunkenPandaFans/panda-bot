99c63d566ff6e66c4dd89e41a82fecd2dfab386a
Jan Ferko <julyloov@gmail.com>
2013-10-18 17:43:54 +0200

    EchoPlugin specs

--- src/main/scala/sk/drunkenpanda/bot/plugins/Plugin.scala
+++ src/main/scala/sk/drunkenpanda/bot/plugins/Plugin.scala
@@ -12,9 +12,20 @@ trait Plugin {
 
 class EchoPlugin extends Plugin {
 
+  private lazy val format = """panda echo (.+), please\.*""".r
+
   override def respond(message: Message) = message match {
-    case PrivateMessage(from, text) => 
-      Option(new PrivateMessage(from, "Echoing message..." + text))
+    case PrivateMessage(from, text) => prepareResponse(from, parseText(text))
+    case _ => None
+  }
+
+  def prepareResponse(to: String, responseMessage: Option[String]) = 
+    for {
+      message <- responseMessage
+    } yield new PrivateMessage(to, s"Echoing message...$message")
+
+  def parseText(text: String) = text match {
+    case format(toEcho) => Some(toEcho)
     case _ => None
   }
 }
---
+++ src/test/scala/sk/drunkenpanda/bot/plugins/EchoPluginSpecs.scala
@@ -0,0 +1,63 @@
+package sk.drunkenpanda.bot.plugins
+
+import org.specs2.mutable._
+import sk.drunkenpanda.bot.Notice
+import sk.drunkenpanda.bot.Ping
+import sk.drunkenpanda.bot.Pong
+import sk.drunkenpanda.bot.PrivateMessage
+import sk.drunkenpanda.bot.Unknown
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ferko
+ * Date: 10/18/13
+ * Time: 4:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class EchoPluginSpecs extends Specification {
+
+  val echoPlugin = new EchoPlugin()
+
+  "EchoPlugin" should {
+    "respond to private messages, that start with 'panda echo'" in {
+       val message = new PrivateMessage("#mychannel", "panda echo this is awesome test, please.")
+       val expectedResponse = new PrivateMessage("#mychannel",
+        "Echoing message...this is awesome test")
+
+        echoPlugin.respond(message) must beSome(expectedResponse)
+      }
+
+      "not respond to other messages than private" in {
+        echoPlugin.respond(Unknown) must beNone
+        echoPlugin.respond(new Ping("pingpong")) must beNone
+        echoPlugin.respond(new Pong("pongping")) must beNone
+        echoPlugin.respond(new Notice("note")) must beNone
+      }
+
+      "parse text, that starts with 'panda echo' and end with ', please'" in {
+        val message = "panda echo this is awesome, please"
+        echoPlugin.parseText(message) must beSome("this is awesome")
+      }
+
+      "not parse text, that does not start with 'panda echo'" in {
+        val message = "this is epic fail, please"
+        echoPlugin.parseText(message) must beNone
+      }
+
+      "not parse text, that does not end with ', please'" in {
+        val message = "panda echo this is even more epic fail"
+        echoPlugin.parseText(message) must beNone
+      }
+
+      "prepare message if there is some text" in {
+        val text = "this is awesome message, it has to be sent."
+        echoPlugin.prepareResponse("toMe", Some(text)) must beSome(
+          new PrivateMessage("toMe", s"Echoing message...$text"))
+      }
+
+      "not prepare message if there is no text" in {
+        echoPlugin.prepareResponse("toMe", None) must beNone
+      }
+  }
+
+}
