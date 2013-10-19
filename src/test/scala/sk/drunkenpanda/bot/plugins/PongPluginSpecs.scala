28e38dc6edc289b7bcb8042ff3909bdd2eb9a51a
Jan Ferko <julyloov@gmail.com>
2013-10-19 20:16:14 +0200

    Added PongPluginSpecs

---
+++ src/test/scala/sk/drunkenpanda/bot/plugins/PongPluginSpecs.scala
@@ -0,0 +1,28 @@
+package sk.drunkenpanda.bot.plugins
+
+import org.specs2.mutable._
+import sk.drunkenpanda.bot.Notice
+import sk.drunkenpanda.bot.Ping
+import sk.drunkenpanda.bot.Pong
+import sk.drunkenpanda.bot.PrivateMessage
+import sk.drunkenpanda.bot.Unknown
+
+class PongPluginSpecs extends Specification {
+
+  val pongPlugin = new PongPlugin()
+
+  "PongPlugin" should {
+    "respond to ping message" in {
+      val hash = "abcde1234"
+      pongPlugin.respond(new Ping(hash)) must beSome(new Pong(hash))
+    }
+
+    "not respond to other type of messages" in {
+      pongPlugin.respond(Unknown) must beNone
+      pongPlugin.respond(new Notice("Message")) must beNone
+      val privMessage = new PrivateMessage("noone", "This is weird msg")
+      pongPlugin.respond(privMessage) must beNone
+      pongPlugin.respond(new Pong("ponging")) must beNone
+    }
+  }
+}
