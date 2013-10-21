99b8237a37463cc86a0e2c12da7f485bf08d5374
Jan Ferko <julyloov@gmail.com>
2013-10-21 15:44:56 +0200

    Expression now works on top of Bigdecimal

--- src/main/scala/sk/drunkenpanda/bot/plugins/calc/Calculator.scala
+++ src/main/scala/sk/drunkenpanda/bot/plugins/calc/Calculator.scala
@@ -2,7 +2,7 @@ package sk.drunkenpanda.bot.plugins.calc
 
 class Calculator {
 
-  def evaluate(exp: Expression): Double = exp match {
+  def evaluate(exp: Expression): BigDecimal = exp match {
     case Number(value) => value
     case UnaryOperator("-", exp) => -evaluate(exp)
     case BinaryOperator("+", left, right) => evaluate(left) + evaluate(right)
--- src/main/scala/sk/drunkenpanda/bot/plugins/calc/Expression.scala
+++ src/main/scala/sk/drunkenpanda/bot/plugins/calc/Expression.scala
@@ -1,7 +1,7 @@
 package sk.drunkenpanda.bot.plugins.calc
 
 sealed trait Expression
-case class Number(value: Double) extends Expression
+case class Number(value: BigDecimal) extends Expression
 case class UnaryOperator(value: String, exp: Expression) extends Expression
 case class BinaryOperator(op: String, leftExp: Expression, rightExp: Expression) 
   extends Expression
