c645c070869e793557953542510ca90ca4721af6
Jan Ferko <julyloov@gmail.com>
2015-05-06 12:22:19 +0200

    Simplify ConnectionSource#write and add error reporting with try

--- src/main/scala/sk/drunkenpanda/bot/io/ConnectionSource.scala
+++ src/main/scala/sk/drunkenpanda/bot/io/ConnectionSource.scala
@@ -3,32 +3,41 @@ package sk.drunkenpanda.bot.io
 import java.io._
 import java.net.Socket
 
+import scala.util.Try
+
 trait ConnectionSource {
 
-  def write(value: String): Unit
+  def write(value: String): Try[Unit]
 
-  def read[A](f: => BufferedReader => A): A
+  def read: Try[String]
 
-  def shutdown: Unit
+  def shutdown: Try[Unit]
 }
 
 class SocketConnectionSource(server: String, port: Int) extends ConnectionSource {
   val socket: Socket = new Socket(server, port)
 
-  def write(value: String) = {
-    val osw = new OutputStreamWriter(socket.getOutputStream)
-    val pw = new PrintWriter(osw)
+  def write(value: String) = for {
+    osw <- Try(new OutputStreamWriter(socket.getOutputStream))
+    pw <- Try(new PrintWriter(osw))
+  } yield {
     pw.write(value)
     pw.write("\n")
     pw.flush()
+    pw.close
+    osw.close
   }
 
-  def read[A](f: => BufferedReader => A) = {
-    val isr = new InputStreamReader(socket.getInputStream)
-    val br = new BufferedReader(isr)
-    f(br)
+  def read = for {
+    isr <- Try(new InputStreamReader(socket.getInputStream))
+    br <- Try(new BufferedReader(isr))
+  } yield {
+    val line = br.readLine
+    br.close
+    isr.close
+    line
   }
 
-  def shutdown: Unit = socket.close()
+  def shutdown = Try(socket.close())
 
 }
