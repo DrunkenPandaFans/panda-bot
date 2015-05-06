d13a21c0e7d421565cce603e5822b8b1f01c2550
Jan Ferko <julyloov@gmail.com>
2015-05-06 13:05:22 +0200

    Add onComplete callback to bot

--- src/main/scala/sk/drunkenpanda/bot/Bot.scala
+++ src/main/scala/sk/drunkenpanda/bot/Bot.scala
@@ -8,13 +8,18 @@ class Bot(ircClient: IrcClient, pluginModule: PluginModule) {
   def start(username: String, nickname: String, realName: String, channels: Seq[String]): Unit = {
     // check and handle errors
     ircClient.connect(username, nickname, realName)
-    val incomingMessages = ircClient.listen(channels)
-    incomingMessages.filter(!_.isEmpty)
-      .map(s => {println(s); Message.parse(s)})
+    ircClient.listen(channels).filter(!_.isEmpty)
+      .map(s => Message.parse(s))
       .map(pluginModule.process(_))
       .subscribe(
-        responseMessages => responseMessages.foreach(ircClient.write(_)),
-        err => println("Error: " + err.getMessage))
+        responseMessages => {
+          println("Incoming Message: " + responseMessages)
+          responseMessages.foreach(ircClient.write(_))
+        },
+        err => {
+          println("Error: " + err.getMessage)
+        },
+        () => stop)
   }
 
   def stop: Unit = {
