772ac91a9775ade13d583b038d7be4b78b27f58a
Jan Ferko <julyloov@gmail.com>
2016-05-31 12:28:28 +0200

    Split project to core and example
    Core subproject now contains only library staff.
    Standalone binary is built from examples subproject.
    This required to move rework sbt configuration.
    
    * move library files to core
    * move App class to examples
    * scala version updated to 2.11.8
    * add cross version build for 2.10.4
    * add scalac options for more warnings
    * add scoverage requirement for minimum of 60 percent code coverage
    * add JavaAppPackaging plugin to examples project

---
+++ core/src/test/scala/sk/drunkenpanda/bot/BotSpecs.scala
@@ -0,0 +1,28 @@
+package sk.drunkenpanda.bot
+
+import java.util.concurrent.Executors
+
+import org.scalatest.{ShouldMatchers, FlatSpec}
+import sk.drunkenpanda.bot.io.{ConnectionSource, IrcClient}
+
+import scala.util.{Success, Try}
+
+class BotSpecs extends FlatSpec with ShouldMatchers {
+
+  class MockConnectionSource extends ConnectionSource {
+    val buffer = collection.mutable.Buffer[String]()
+
+    def write(value: String): Try[Unit] = {
+      buffer += value
+      Success()
+    }
+
+    def read: Try[String] = Success("PONG")
+
+    def shutdown: Try[Unit] = Success()
+  }
+
+
+  class MockIrcClient extends IrcClient(new MockConnectionSource(), Executors.newSingleThreadExecutor())
+
+}
