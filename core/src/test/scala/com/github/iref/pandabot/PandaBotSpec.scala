faf90ce7da29d7158aa576123bd9ff06bd2cc148
Jan Ferko <julyloov@gmail.com>
2016-06-08 13:08:02 +0200

    Add base specification class and scalacheck config

---
+++ core/src/test/scala/com/github/iref/pandabot/PandaBotSpec.scala
@@ -0,0 +1,35 @@
+package com.github.iref.pandabot
+
+import com.github.iref.pandabot.protocol.Message
+import org.scalacheck.Arbitrary
+import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.prop.{ Configuration, GeneratorDrivenPropertyChecks }
+
+trait PropertyCheckerSettings extends Configuration {
+
+  lazy val checkConfiguration: PropertyCheckConfig =
+    PropertyCheckConfig(
+      minSuccessful = 200,
+      minSize = 0,
+      maxSize = 20
+    )
+
+}
+
+/**
+ * Opinionated specification trait stack to improve consistency and
+ * provide boilerplate in tests.
+ */
+abstract class PandaBotSpec(component: String) extends FlatSpec
+    with Matchers
+    with GeneratorDrivenPropertyChecks
+    with PropertyCheckerSettings
+    with MessageGenerators {
+
+  implicit override val generatorDrivenConfig = checkConfiguration
+
+  implicit val messageArbitrary: Arbitrary[Message] = Arbitrary(genMessage)
+
+  behavior of component
+
+}
